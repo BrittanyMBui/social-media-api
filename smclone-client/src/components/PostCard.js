07475f861d8769cc78e35eecfe18680df508745a
brittanymbui <brittymbui@gmail.com>
2021-05-23 21:36:52 -0700

    Adds link to the timestamp on posts, adds styling to posts

--- smclone-client/src/components/PostCard.js
+++ smclone-client/src/components/PostCard.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Card, Icon, Label, Image } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 import moment from 'moment';
 
 function PostCard({ post: { body, createdAt, id, username, likeCount, commentCount, likes }}) {
 
     return (
-        <Card>
+        <Card fluid>
         <Card.Content>
             <Image
             floated='right'
@@ -13,7 +14,7 @@ function PostCard({ post: { body, createdAt, id, username, likeCount, commentCou
             src='https://react.semantic-ui.com/images/avatar/large/molly.png'
             />
             <Card.Header>{username}</Card.Header>
-            <Card.Meta>{moment(createdAt).fromNow()}</Card.Meta>
+            <Card.Meta as={Link} to={`/posts/${id}`}>{moment(createdAt).fromNow(true)}</Card.Meta>
             <Card.Description>
             {body}
             </Card.Description>
