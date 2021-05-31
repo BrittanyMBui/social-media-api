e02ca724454db2a2b10c2abe106561c6c880d25e
brittanymbui <brittymbui@gmail.com>
2021-05-30 18:52:59 -0700

    Adds functionality to like button

--- smclone-client/src/components/PostCard.js
+++ smclone-client/src/components/PostCard.js
@@ -1,16 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Card, Icon, Label, Image, Button } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
 
+import { AuthContext } from '../context/auth'
+import LikeButton from './LikeButton'
+
 function PostCard({ post: { body, createdAt, id, username, likeCount, commentCount, likes }}) {
 
-    function likePost() {
-        console.log('Liked');
-    }
-    function commentOnPost() {
-        console.log('commented!');
-    }
+    const { user } = useContext(AuthContext);
 
     return (
         <Card fluid>
@@ -27,15 +25,8 @@ function PostCard({ post: { body, createdAt, id, username, likeCount, commentCou
             </Card.Description>
         </Card.Content>
         <Card.Content extra>
-            <Button as='div' labelPosition='right' onClick={likePost}>
-            <Button color='teal' basic>
-                <Icon name='heart' />
-            </Button>
-            <Label basic color='teal' pointing='left'>
-                {likeCount}
-            </Label>
-            </Button>
-            <Button as='div' labelPosition='right' onClick={commentOnPost}>
+            <LikeButton user={user} post={{ id, likes, likeCount }} />
+            <Button labelPosition='right' as={Link} to={`/posts/${id}`}>
             <Button color='blue' basic>
                 <Icon name='comment' />
             </Button>
@@ -43,6 +34,16 @@ function PostCard({ post: { body, createdAt, id, username, likeCount, commentCou
                 {commentCount}
             </Label>
             </Button>
+            {user &&  user.username === username && (
+                <Button 
+                    as="div" 
+                    color="gray"
+                    floated="right"
+                    onClick={() => console.log('delete post')}
+                >
+                    <Icon name="trash" style={{ margin: 0}}/>
+                </Button>
+            )}
         </Card.Content>
         </Card>
     )
